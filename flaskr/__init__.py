f8b638876c845df65fba0a646c09235a2c4c7d8f
Colin Goutte <cgte@bk.ru>
2018-08-21 14:49:16 +0200

    Generate pid when starting the app

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -11,13 +11,16 @@ def create_app(test_config=None):
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
+
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
-
+    pid = os.getpid()
+    with open('flask.pid', 'w') as f:
+        f.write('%d' % pid)
     # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
